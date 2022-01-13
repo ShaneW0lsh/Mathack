24e14cc52f1b7658fcf3b89ec7cd3c0e38a2c5a6
ShaneW0lsh <shanebetterfather@gmail.com>
2022-01-13 18:15:11 +0300

    added 'getData()', 'addData()', 'getInstance()' DataHolder methods

--- app/src/main/java/com/example/speedcalc/DataHolder.java
+++ app/src/main/java/com/example/speedcalc/DataHolder.java
@@ -1,7 +1,18 @@
 package com.example.speedcalc;
 
 public class DataHolder {
-    private ArrayList<Expression>;
+    private ArrayList<Expression> data;
+    
+    private static final DataHolder holder = new DataHolder();
+    public static DataHolder getInstance() {
+        return this.holder;
+    }
 
+    public ArrayList<Expression> getData() {
+        return this.data;
+    }
 
+    public void addData(Expression expr) {
+        this.data.add(expr);
+    }
 }
