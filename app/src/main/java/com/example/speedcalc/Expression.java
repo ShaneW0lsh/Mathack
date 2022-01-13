51d5c72e4431b7e8a06e6c88144363ad471736c6
ShaneW0lsh <shanebetterfather@gmail.com>
2022-01-13 17:31:28 +0300

    added a new activity (AllTasksActivity), class Expression

---
+++ app/src/main/java/com/example/speedcalc/Expression.java
@@ -0,0 +1,23 @@
+package com.example.speedcalc;
+
+public class Expression {
+    private String operation;
+
+    private String answer;
+    private String fVal, sVal;
+
+    public Experission(String fVal, String sVal, String operation) {
+        this.fVal = fVal;
+        this.sVal = sVal;
+        this.operation = operation;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+
+    public String[] getValues() {
+        String[] ret = {this.fVal, this.sVal};
+        return ret;
+    }
+}
