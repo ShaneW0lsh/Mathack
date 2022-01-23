f3c12c35967619799dc9ba71c2efe99d7367bdb0
ShaneW0lsh <shanebetterfather@gmail.com>
2022-01-23 21:26:38 +0300

    added toast msg in 'onCreate()' method

--- app/src/main/java/com/example/mathack/MainActivity.java
+++ app/src/main/java/com/example/mathack/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
         bntSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //does nothing for now
+                Toast.makeText(MainActivity.this, "in development", Toast.LENGTH_SHORT).show();
             }
         });
 
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //does nothing for now
+                Toast.makeText(MainActivity.this, "in development", Toast.LENGTH_SHORT).show();
             }
         });
     }
