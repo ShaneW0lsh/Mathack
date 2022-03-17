f05dc57e3b47db9db651d2262492c6c34a06a18d
Shane Walsh <shanebetterfather@gmail.com>
2022-03-17 18:33:20 +0300

    Do initialization for all components

--- app/src/main/java/com/shane/mathack/fragments/PlayFragment.java
+++ app/src/main/java/com/shane/mathack/fragments/PlayFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,8 @@ import com.shane.mathack.activities.TaskSessionActivity;
 public class PlayFragment extends Fragment {
 
     Button mStartButton;
+    EditText mDepthEditText, mConstrEditText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_play, container, false);
@@ -28,6 +31,8 @@ public class PlayFragment extends Fragment {
 
     private void initViews() {
         mStartButton = (Button) getView().findViewById(R.id.start_button);
+        mDepthEditText = (EditText) getView().findViewById(R.id.depthEditText);
+        mConstrEditText = (EditText) getView().findViewById(R.id.consrEditText);
     }
 
     private void manageListeners() {
